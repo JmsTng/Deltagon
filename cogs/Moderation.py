524fe17a0ff6bbddd6410832ce1a63570bd0aa90
JmsTng <95390086+JmsTng@users.noreply.github.com>
2021-12-17 15:45:04 -0500

    Finish Moderation.py
    Completed Moderation.py by adding in the Kick and Ban commands.

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -2,7 +2,24 @@ from discord.ext import commands
 import discord, os, time
 
 class Moderation(commands.Cog):
-  async def __init__(self):
-    self.bot = bot
-  
-  async def
+	@commands.has_permissions(kick_members=True)
+	@commands.command(pass_context = True)
+	async def kick(ctx, member: commands.MemberConverter, *, why=None):
+		if why == None:
+			await ctx.channel.send("Please specify a reason!")
+		else:
+			try:
+				await member.kick(reason=why)
+			except:
+				await ctx.channel.send("There was an issue")
+
+	@commands.has_permissions(ban_members=True)
+	@commands.command(pass_context = True)
+	async def ban(ctx, member: commands.MemberConverter, *, why=None):
+		if why == None:
+			await ctx.channel.send("Please specify a reason!")
+		else:
+			try:
+				await member.ban(reason=why)
+			except:
+				await ctx.channel.send("There was an issue")
