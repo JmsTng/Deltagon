87cb7d962333d834eb63c52b7faf79802a4bc924
JmsTng <95390086+JmsTng@users.noreply.github.com>
2022-01-31 13:58:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+# package imports
+from nextcord.ext import commands
+from replit import db
+import os, neversleep, discord, io, aiohttp
+
+# cog imports
+from "../cogs/Command.py" import CommandPerms
+from "../cogs/Messages.py" import Messages
 from "../cogs/Moderation.py" import Moderation
