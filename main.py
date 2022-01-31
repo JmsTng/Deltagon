a7497e8e37368bf2f0999dc9e38ccd6eb0f97ab3
JmsTng <95390086+JmsTng@users.noreply.github.com>
2022-01-31 14:07:58 -0500

    Update main.py
    Made bot operational

--- main.py
+++ main.py
@@ -7,3 +7,30 @@ import os, neversleep, discord, io, aiohttp
 from "../cogs/Command.py" import CommandPerms
 from "../cogs/Messages.py" import Messages
 from "../cogs/Moderation.py" import Moderation
+
+# initialize bot
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=["delta:", "d:"], activity=discord.Game(name="delta:help"), intents=discord.Intents.default())
+
+# constant vars
+token = os.getenv("TOKEN")
+cmdl = ["kick", "ban", "say", "delete", "membercount", "invite", "dump", "bookmark"]
+
+# initialize blocklist
+try:
+	db["blocklist"]
+except:
+	db["blocklist"] = {}
+	for i in commandlist:
+		try:
+			db["blocklist"][i]
+		except:
+			db["blocklist"][i] = []
+
+# wakeup call
+@bot.event
+async def on_ready():
+  print(f"Ready! Started as {bot.user}.")
+  
+bot.run(token)  
